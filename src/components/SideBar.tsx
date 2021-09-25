457c383a9b45064801cfd1f70616d60a7c120457
Erico Vasconcelos <erico@ettbrasil.com.br>
2021-09-25 04:43:47 -0300

    [ADD] Separated logics into Content and SideBar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+
+import { Button } from './Button';
+
+export type Genre = {
+  id: number,
+  title: string,
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+}
+
+interface SideBarProps {
+  genres: Array<Genre>,
+  onSelectGenre : (id:number)=> void,
+  selectedGenreId: number,
+}
+
+interface SelectButton {
+  iconName: string;
+}
+
+export function SideBar({genres, onSelectGenre, selectedGenreId}: SideBarProps) {
+
+
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+
+  );
 }
